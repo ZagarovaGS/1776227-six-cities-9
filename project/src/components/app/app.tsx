f34c1130ea583db010c943c724b4c5f6852101a6
Гульнара Загарова <n459sverk@gmail.com>
2022-02-13 10:34:08 +0300

    разбили проект на компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import Main from '../main/main';
+
+type WellcomeScreenProps = {
+  offersCount: number;
+}
+
+function App({ offersCount }: WellcomeScreenProps): JSX.Element {
+  return (
+    <div>
+      <Main offersCount={offersCount} />
+    </div>
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const offersCount = 122;
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App offersCount={offersCount} />
   </React.StrictMode>,
   document.getElementById('root'));
